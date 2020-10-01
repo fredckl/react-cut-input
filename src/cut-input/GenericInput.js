1771c27e81e892547e7cd5ce560e5748ca6b4382
Frédéric KOLLER <koller.f@gmail.com>
2020-10-01 07:21:14 +0200

    move file

--- src/cut-input/GenericInput.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const GenericInput = (props) => <input {...props}/>;
-
-export default GenericInput
---
+++ src/cut-input/components/GenericInput.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GenericInput = (props) => <input {...props}/>;
+
+export default GenericInput;
